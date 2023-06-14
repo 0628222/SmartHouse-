fcdbd8e95824bd186150da23fb247bd0c37aeb59
Peter <0628222@schoolsnet.act.edu.au>
2023-06-14 15:23:20 +1000

    finializing

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -167,16 +167,16 @@ void lockDoorSecurity() {
 Servo myservo;  // create servo object to control a servo
 
 }
-  /*for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
- */  
-  /* for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+   
+   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
    delay(15);         
 }
-*/ 
+ 
 
 /* using line sensor it detects when somebody is near by and then opens the door using a DC motor
   @params
